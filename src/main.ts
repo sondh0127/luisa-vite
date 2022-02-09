f93d7dcf37d24055e5621f805f57561924adb1fb
Son Hong Do <sondh0127@gmail.com>
2022-02-09 10:32:51 +0700

    link local for dev

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Luisa from 'luisa-vue'
+import 'luisa-vue/dist/style.css'
 
 const app = createApp(App)
 app.use(router)
